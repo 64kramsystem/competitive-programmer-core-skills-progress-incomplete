23fc989db0fabfe0608f202be928001fc76b3cff
Saverio Miroddi <saverio.pub2@gmail.com>
2021-07-02 00:42:33 +0200

    Add Week 4 assignment, problem 1 (fastest)

--- week_4_common_struggles_2/frequent_symbol.rb
+++ week_4_common_struggles_2/frequent_symbol.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+# With suggestion from the professor.
+
 require 'stringio'
 
 def parse_input(input)
@@ -10,57 +12,53 @@ def parse_input(input)
   [string_chars, subarrays]
 end
 
-def find_frequency_table(string_chars, start, end_, frequency_table)
-  string_chars[(start - 1)..(end_ - 1)].each do |char|
-    char_i = char.ord - 'a'.ord
-    frequency_table[char_i] += 1
+def build_progressive_frequency_tables(string_chars)
+  tables = Array.new(26) { [nil] * string_chars.size }
+
+  string_chars.each_with_index do |char, i|
+    tables.each do |table|
+      if i == 0
+        table[i] = 0
+      else
+        table[i] = table[i - 1]
+      end
+    end
+
+    tables[char.ord - 'a'.ord][i] += 1
   end
 
-  frequency_table
+  tables
 end
 
-def find_most_frequent_char(frequency_table)
-  max_mfc_count = 0
-  max_mfc_index = -1
+def compute_occurrencies(frequency_table, start, end_)
+  # Normalize
+  start, end_ = start - 1, end_ - 1
 
-  frequency_table.each_with_index do |char_frequency, char_index|
-    if char_frequency >= max_mfc_count
-      max_mfc_count = char_frequency
-      max_mfc_index = char_index
-    end
-  end
+  start_value = start == 0 ? 0 : frequency_table[start - 1]
 
-  ('a'.ord + max_mfc_index).chr
+  frequency_table[end_] - start_value
 end
 
 def main(input, output)
   string_chars, subarrays = parse_input(input)
 
-  subarrays = subarrays
-    .each_with_index
-    .map { |subarray, i| [subarray, i] }
-    .sort_by { |subarray, _| subarray }
+  progressive_frequency_tables = build_progressive_frequency_tables(string_chars)
 
-  result = [nil] * subarrays.size
+  subarrays.each do |(start, end_)|
+    max_occurrences = 0
+    best_char_i = nil
 
-  subarrays.inject([0, 0]) do |(previous_start, previous_end, previous_frequency_table), ((start, end_), subarray_i)|
-    if start != previous_start
-      previous_frequency_table = [0] * 26
-      shorter_start = start
-    else
-      shorter_start = previous_end + 1
+    progressive_frequency_tables.each_with_index do |frequency_table, char_i|
+      occurrences = compute_occurrencies(frequency_table, start, end_)
+
+      if occurrences >= max_occurrences
+        best_char_i = char_i
+        max_occurrences = occurrences
+      end
     end
-# print "I:#{subarray_i} S:#{start} E:#{end_} PFT:#{previous_frequency_table} => "
-    current_frequency_table = find_frequency_table(string_chars, shorter_start, end_, previous_frequency_table)
-# puts "CFT:#{current_frequency_table}"
-    most_frequent_char = find_most_frequent_char(current_frequency_table)
 
-    result[subarray_i] = most_frequent_char
-
-    [start, end_, current_frequency_table]
+    puts ('a'.ord + best_char_i).chr
   end
-
-  result.each { |char| output.puts char }
 end
 
 input = STDIN
