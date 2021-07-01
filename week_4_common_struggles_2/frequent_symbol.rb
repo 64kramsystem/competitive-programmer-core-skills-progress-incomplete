26d3cfbdc832aabca5ee740052c26707e7d20f86
Saverio Miroddi <saverio.pub2@gmail.com>
2021-07-01 23:49:40 +0200

    Add Week 4 assignment, problem 1 (faster)

--- week_4_common_struggles_2/frequent_symbol.rb
+++ week_4_common_struggles_2/frequent_symbol.rb
@@ -10,14 +10,16 @@ def parse_input(input)
   [string_chars, subarrays]
 end
 
-def find_most_frequent_char(string_chars, start, end_)
-  frequency_table = [0] * 26
-
+def find_frequency_table(string_chars, start, end_, frequency_table)
   string_chars[(start - 1)..(end_ - 1)].each do |char|
     char_i = char.ord - 'a'.ord
     frequency_table[char_i] += 1
   end
 
+  frequency_table
+end
+
+def find_most_frequent_char(frequency_table)
   max_mfc_count = 0
   max_mfc_index = -1
 
@@ -34,10 +36,31 @@ end
 def main(input, output)
   string_chars, subarrays = parse_input(input)
 
-  subarrays.each do |start, end_|
-    most_frequent_char = find_most_frequent_char(string_chars, start, end_)
-    output.puts most_frequent_char
+  subarrays = subarrays
+    .each_with_index
+    .map { |subarray, i| [subarray, i] }
+    .sort_by { |subarray, _| subarray }
+
+  result = [nil] * subarrays.size
+
+  subarrays.inject([0, 0]) do |(previous_start, previous_end, previous_frequency_table), ((start, end_), subarray_i)|
+    if start != previous_start
+      previous_frequency_table = [0] * 26
+      shorter_start = start
+    else
+      shorter_start = previous_end + 1
+    end
+# print "I:#{subarray_i} S:#{start} E:#{end_} PFT:#{previous_frequency_table} => "
+    current_frequency_table = find_frequency_table(string_chars, shorter_start, end_, previous_frequency_table)
+# puts "CFT:#{current_frequency_table}"
+    most_frequent_char = find_most_frequent_char(current_frequency_table)
+
+    result[subarray_i] = most_frequent_char
+
+    [start, end_, current_frequency_table]
   end
+
+  result.each { |char| output.puts char }
 end
 
 input = STDIN
